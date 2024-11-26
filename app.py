b5c8ded84ad66b55e5568a038968676c49595395
Tabassum Sultana <sultanatabassum837@gmail.com>
2024-11-26 21:36:17 +0600

    second

--- app.py
+++
@@ -1,8 +0,0 @@
-course_name = "Python Programming"
-print(len(course_name))
-print(course_name[0])
-print(course_name[-1])
-print(course_name[0:3])
-print(course_name[0:])
-print(course_name[:3])
-print(course_name[:])
---
+++ input.py
@@ -0,0 +1,31 @@
+# arithmetic operator
+x = 10
+y = 12
+add = x+y
+print(add)
+sub = x-y
+print(sub)
+mul = x*y
+print(mul)
+div = x/y
+print(div)
+p = x**2
+print(p)
+mod = x % y
+print(mod)
+# logical operator
+A = True
+B = False
+print(A and B)
+print(A or B)
+print(not A)
+# bitwise operator
+# compliment operator
+M = 10
+N = 4
+print(M & N)
+print(M | N)
+print(~N)
+print(M ^ N)
+print(M << N)
+print(M >> N)
